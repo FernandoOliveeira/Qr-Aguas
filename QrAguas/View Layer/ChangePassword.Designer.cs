04106b535f34e3be2c31945b48d86a1d630d1a50
Fernando <fernando.silva789@etec.sp.gov.br>
2020-01-14 16:16:42 -0300

    ChangePassword: Mudanças no design para que não seja possível maximizar, aumentar ou diminuir o tamanho do form

--- QrAguas/View Layer/ChangePassword.Designer.cs
+++ QrAguas/View Layer/ChangePassword.Designer.cs
@@ -293,7 +293,9 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(156)))), ((int)(((byte)(188)))));
             this.ClientSize = new System.Drawing.Size(634, 361);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ChangePassword";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Qr Águas - Alterar Senha";
