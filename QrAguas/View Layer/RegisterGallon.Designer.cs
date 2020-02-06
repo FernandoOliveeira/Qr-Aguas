6c1a3aeb7a41e69e51d4236aa5015dcaba8e24ff
Fernando <fernando.silva789@etec.sp.gov.br>
2020-02-06 15:34:17 -0300

    Form RegisterGallon: Adicionado os campos para a realização do cadastro do galão

--- QrAguas/View Layer/RegisterGallon.Designer.cs
+++ QrAguas/View Layer/RegisterGallon.Designer.cs
@@ -29,20 +29,280 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterGallon));
+            this.txtFornecedor = new System.Windows.Forms.TextBox();
+            this.lblFornecedor = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelFornecedor = new System.Windows.Forms.Panel();
+            this.lblAvisoFornecedor = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtCodProduto = new System.Windows.Forms.TextBox();
+            this.lblCodProduto = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.panelPrecoCompra = new System.Windows.Forms.Panel();
+            this.txtPrecoCompra = new System.Windows.Forms.TextBox();
+            this.lblPrecoCompra = new System.Windows.Forms.Label();
+            this.panelPrecoEntrega = new System.Windows.Forms.Panel();
+            this.txtPrecoVenda = new System.Windows.Forms.TextBox();
+            this.lblPrecoVenda = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panelFornecedor.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panelPrecoCompra.SuspendLayout();
+            this.panelPrecoEntrega.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtFornecedor
+            // 
+            this.txtFornecedor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFornecedor.Location = new System.Drawing.Point(9, 26);
+            this.txtFornecedor.Name = "txtFornecedor";
+            this.txtFornecedor.Size = new System.Drawing.Size(198, 20);
+            this.txtFornecedor.TabIndex = 0;
+            // 
+            // lblFornecedor
+            // 
+            this.lblFornecedor.AutoSize = true;
+            this.lblFornecedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFornecedor.ForeColor = System.Drawing.Color.White;
+            this.lblFornecedor.Location = new System.Drawing.Point(6, 6);
+            this.lblFornecedor.Name = "lblFornecedor";
+            this.lblFornecedor.Size = new System.Drawing.Size(85, 17);
+            this.lblFornecedor.TabIndex = 1;
+            this.lblFornecedor.Text = "Fornecedor:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.1308F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.04795F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.96575F));
+            this.tableLayoutPanel1.Controls.Add(this.panelPrecoCompra, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelFornecedor, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panelPrecoEntrega, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.21348F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.34082F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.72285F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.72285F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 411);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panelFornecedor
+            // 
+            this.panelFornecedor.Controls.Add(this.lblAvisoFornecedor);
+            this.panelFornecedor.Controls.Add(this.txtFornecedor);
+            this.panelFornecedor.Controls.Add(this.lblFornecedor);
+            this.panelFornecedor.Location = new System.Drawing.Point(3, 81);
+            this.panelFornecedor.Name = "panelFornecedor";
+            this.panelFornecedor.Size = new System.Drawing.Size(210, 65);
+            this.panelFornecedor.TabIndex = 3;
+            // 
+            // lblAvisoFornecedor
+            // 
+            this.lblAvisoFornecedor.AutoSize = true;
+            this.lblAvisoFornecedor.ForeColor = System.Drawing.Color.White;
+            this.lblAvisoFornecedor.Location = new System.Drawing.Point(15, 49);
+            this.lblAvisoFornecedor.Name = "lblAvisoFornecedor";
+            this.lblAvisoFornecedor.Size = new System.Drawing.Size(58, 13);
+            this.lblAvisoFornecedor.TabIndex = 2;
+            this.lblAvisoFornecedor.Text = "Obrigatório";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtCodProduto);
+            this.panel1.Controls.Add(this.lblCodProduto);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 69);
+            this.panel1.TabIndex = 4;
+            // 
+            // txtCodProduto
+            // 
+            this.txtCodProduto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCodProduto.Location = new System.Drawing.Point(9, 26);
+            this.txtCodProduto.Name = "txtCodProduto";
+            this.txtCodProduto.Size = new System.Drawing.Size(198, 20);
+            this.txtCodProduto.TabIndex = 0;
+            // 
+            // lblCodProduto
+            // 
+            this.lblCodProduto.AutoSize = true;
+            this.lblCodProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodProduto.ForeColor = System.Drawing.Color.White;
+            this.lblCodProduto.Location = new System.Drawing.Point(6, 6);
+            this.lblCodProduto.Name = "lblCodProduto";
+            this.lblCodProduto.Size = new System.Drawing.Size(130, 17);
+            this.lblCodProduto.TabIndex = 1;
+            this.lblCodProduto.Text = "Código do Produto:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtNome);
+            this.panel2.Controls.Add(this.lblNome);
+            this.panel2.Location = new System.Drawing.Point(219, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 69);
+            this.panel2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Obrigatório";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNome.Location = new System.Drawing.Point(9, 26);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(174, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(6, 6);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(49, 17);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // panelPrecoCompra
+            // 
+            this.panelPrecoCompra.Controls.Add(this.txtPrecoCompra);
+            this.panelPrecoCompra.Controls.Add(this.lblPrecoCompra);
+            this.panelPrecoCompra.Location = new System.Drawing.Point(411, 3);
+            this.panelPrecoCompra.Name = "panelPrecoCompra";
+            this.panelPrecoCompra.Size = new System.Drawing.Size(170, 69);
+            this.panelPrecoCompra.TabIndex = 6;
+            // 
+            // txtPrecoCompra
+            // 
+            this.txtPrecoCompra.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrecoCompra.Location = new System.Drawing.Point(9, 26);
+            this.txtPrecoCompra.Name = "txtPrecoCompra";
+            this.txtPrecoCompra.Size = new System.Drawing.Size(158, 20);
+            this.txtPrecoCompra.TabIndex = 0;
+            // 
+            // lblPrecoCompra
+            // 
+            this.lblPrecoCompra.AutoSize = true;
+            this.lblPrecoCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoCompra.ForeColor = System.Drawing.Color.White;
+            this.lblPrecoCompra.Location = new System.Drawing.Point(6, 6);
+            this.lblPrecoCompra.Name = "lblPrecoCompra";
+            this.lblPrecoCompra.Size = new System.Drawing.Size(122, 17);
+            this.lblPrecoCompra.TabIndex = 1;
+            this.lblPrecoCompra.Text = "Preço de Compra:";
+            // 
+            // panelPrecoEntrega
+            // 
+            this.panelPrecoEntrega.Controls.Add(this.txtPrecoVenda);
+            this.panelPrecoEntrega.Controls.Add(this.lblPrecoVenda);
+            this.panelPrecoEntrega.Location = new System.Drawing.Point(411, 81);
+            this.panelPrecoEntrega.Name = "panelPrecoEntrega";
+            this.panelPrecoEntrega.Size = new System.Drawing.Size(170, 65);
+            this.panelPrecoEntrega.TabIndex = 7;
+            // 
+            // txtPrecoVenda
+            // 
+            this.txtPrecoVenda.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrecoVenda.Location = new System.Drawing.Point(9, 26);
+            this.txtPrecoVenda.Name = "txtPrecoVenda";
+            this.txtPrecoVenda.Size = new System.Drawing.Size(158, 20);
+            this.txtPrecoVenda.TabIndex = 0;
+            // 
+            // lblPrecoVenda
+            // 
+            this.lblPrecoVenda.AutoSize = true;
+            this.lblPrecoVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoVenda.ForeColor = System.Drawing.Color.White;
+            this.lblPrecoVenda.Location = new System.Drawing.Point(6, 6);
+            this.lblPrecoVenda.Name = "lblPrecoVenda";
+            this.lblPrecoVenda.Size = new System.Drawing.Size(123, 17);
+            this.lblPrecoVenda.TabIndex = 1;
+            this.lblPrecoVenda.Text = "Preço de Entrega:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Obrigatório";
             // 
             // RegisterGallon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(156)))), ((int)(((byte)(188)))));
             this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RegisterGallon";
             this.Text = "Qr Águas - Cadastrar Galão";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panelFornecedor.ResumeLayout(false);
+            this.panelFornecedor.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panelPrecoCompra.ResumeLayout(false);
+            this.panelPrecoCompra.PerformLayout();
+            this.panelPrecoEntrega.ResumeLayout(false);
+            this.panelPrecoEntrega.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFornecedor;
+        private System.Windows.Forms.Label lblFornecedor;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panelFornecedor;
+        private System.Windows.Forms.Label lblAvisoFornecedor;
+        private System.Windows.Forms.Panel panelPrecoCompra;
+        private System.Windows.Forms.TextBox txtPrecoCompra;
+        private System.Windows.Forms.Label lblPrecoCompra;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodProduto;
+        private System.Windows.Forms.Label lblCodProduto;
+        private System.Windows.Forms.Panel panelPrecoEntrega;
+        private System.Windows.Forms.TextBox txtPrecoVenda;
+        private System.Windows.Forms.Label lblPrecoVenda;
     }
 }
