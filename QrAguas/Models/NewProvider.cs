06e304ea0fb19a99c1caf344f57c9ac487946816
Fernando N53Ta <fernando.silva789@etec.sp.gov.br>
2020-02-19 21:46:03 -0300

    Criado a classe NewProvider para receber os dados do form RegisterProviders

---
+++ QrAguas/Models/NewProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrAguas.Models
+{
+    class NewProvider
+    {
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Complemento { get; set; }
+        public string Uf { get; set; }
+        public string Telefone { get; set; }
+        public string Cep { get; set; }
+        public string Email { get; set; }
+    }
+}
