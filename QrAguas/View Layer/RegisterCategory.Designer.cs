c13b3efa47ff9f1c95a15863cebf7081962a3d52
Fernando N53Ta <fernando.silva789@etec.sp.gov.br>
2020-02-26 19:22:59 -0300

    Criado o form RegisterCategory e iniciado o design

---
+++ QrAguas/View Layer/RegisterCategory.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace QrAguas.View_Layer
+{
+    partial class RegisterCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterCategory));
+            this.SuspendLayout();
+            // 
+            // RegisterCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 362);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RegisterCategory";
+            this.Text = "Qr Águas - Cadastrar Categoria";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
