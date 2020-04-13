2f8615bd97da9077b62c42f99a4ac5826a27edf2
Fernando N53Ta <fernando.silva789@etec.sp.gov.br>
2020-04-13 17:51:26 -0300

    Form Login: Melhorado a eficácia do método PressionarEnter

--- QrAguas/ViewLayer/Login.cs
+++ QrAguas/ViewLayer/Login.cs
@@ -70,7 +70,7 @@ namespace QrAguas.View_Layer
               }
             else
             {
-                MessageBox.Show("Usuario ou senha inválidos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Usuario ou senha inválidos", "Usuario ou senha inválidos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
 
@@ -83,31 +83,7 @@ namespace QrAguas.View_Layer
 
         private void PressionarEnter_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyValue == 13)
-            {
-                // Gera um HashMd5 e armazena na variavel senha
-                string senha = functions.GerarMd5(txtSenha.Text.Trim());
-
-                if (functions.VerificarLogin(txtUsuario.Text.Trim(), senha))
-                {
-
-                    NomeUsuario = txtUsuario.Text.Trim();
-                    TipoUsuario = functions.VerificarTipoUsuario(NomeUsuario);
-                    IdUsuario = functions.VerificarIdUsuario(NomeUsuario);
-
-                    // Abrir o form MainForm e fechar o atual
-                    this.Close();
-                    thread = new Thread(AbrirMainForm);
-                    thread.SetApartmentState(ApartmentState.STA);
-                    thread.Start();
-                }
-                else
-                {
-                    MessageBox.Show("Usuario ou senha inválidos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-
-            }
+            ActiveForm.AcceptButton = btnEntrar;
 
         }
 
