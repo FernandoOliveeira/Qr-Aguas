779d728fb409ef4bc46cb45330b4ed060fca6e00
Fernando <fernando.silva789@etec.sp.gov.br>
2020-05-23 18:26:53 -0300

    Controls: Adicionado a classe UpdateProvidersMethods, para que a mesma receba os métodos de seu respectivo form.

---
+++ QrAguas/Controls/UpdateProviderMethods.cs
@@ -0,0 +1,18 @@
+﻿using QrAguas.BusinessLayer;
+using QrAguas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrAguas.Controls
+{
+    class UpdateProviderMethods : BlDataVerification
+    {
+        public bool AtualizarFornecedor(NewProvider fornecedor)
+        {
+
+        }
+    }
+}
